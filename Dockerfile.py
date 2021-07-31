551ccd6863d735ad8c40495b4f97e1494f5cdcae
Daniel <daniel@developerdan.com>
2021-07-31 02:04:15 +0000

    Try a simple build first

--- Dockerfile.py
+++ Dockerfile.py
@@ -101,6 +101,7 @@ def build_dockerfiles(args) -> bool:
         print(" ::: Skipping Dockerfile building")
         return all_success
 
+    run_and_stream_command_output('docker buildx create --use', [], True)
     for debian_version in BUILD_VARS['debian_versions']:
         all_success = build('pihole', debian_version, args['-t'], args['--no-cache'], args['-v']) and all_success
         if not all_success and args['--fail-fast']:
@@ -132,7 +133,6 @@ def build(docker_repo: str, debian_version: str, show_time: bool, no_cache: bool
     build_env['PIHOLE_VERSION'] = FTL_VERSION
     build_env['DEBIAN_VERSION'] = debian_version
     write_bake_config(BUILD_VARS['archs'], debian_version)
-    run_and_stream_command_output('docker buildx create --use', build_env, verbose)
     print(f' ::: Building {create_tag}')
     build_command = f'{time_arg} docker buildx bake --file build.yml --file {BAKE_CONFIG_FILE} {cache_arg} --push'
     run_and_stream_command_output(build_command + ' --print', build_env, verbose)
