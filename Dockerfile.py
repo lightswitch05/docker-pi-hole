3ec7f03561c7d0fe96026e81550d1370399615bb
Adam Warner <me@adamwarner.co.uk>
2021-08-04 12:09:26 +0100

    No need for vars.sh as it's sourced by test.sh try `--local` on the buildx command
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- Dockerfile.py
+++ Dockerfile.py
@@ -101,7 +101,7 @@ def build_dockerfiles(args) -> bool:
         print(" ::: Skipping Dockerfile building")
         return all_success
 
-    run_and_stream_command_output('docker buildx create --use --name pihole-build', os.environ.copy(), True)
+    run_and_stream_command_output('docker buildx create --use --name pihole-build --local', os.environ.copy(), True)
     for debian_version in BUILD_VARS['debian_versions']:
         all_success = build('pihole', debian_version, args['-t'], args['--no-cache'], args['-v']) and all_success
         if not all_success and args['--fail-fast']:
